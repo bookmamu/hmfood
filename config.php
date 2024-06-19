0954e823d45c0db35a5fc767f27082bc04d4f0d7
Mitesh Patel <bookmamu@gmail.com>
2024-06-19 08:48:49 +0530

    Updated URL for Config

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Kolkata");
 
-define('APPLICATION_URL', 'http://localhost:555/');
+define('APPLICATION_URL', 'https://hmfood.azurewebsites.net/');
 define('APPLICATION_SUBDIR', '/');
 define('ADMIN_EMAIL', 'support@hmfoodwala.com');
 define('CAPTCHA_SITEKEY', '6Ld-Y5oaAAAAANBGR1FzSNTyF9oIN-M5XrDUFwZd	');
